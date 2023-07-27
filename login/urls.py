a1f2ea484a98b88dbdd6fbc16417d7e4c18edcb4
manhunter7-2 <thomas.piquet7@gmail.com>
2023-07-27 20:18:33 +0200

    login creation wip

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("test", views.create, name="create"),
+    path("create", views.createAccount, name="createAccount")
 ]
--- login/views.py
+++ login/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect, csrf_exempt
+import environ
+import hashlib
+from login.models import Login
+
+env = environ.Env()
+environ.Env.read_env()
+salt = env("SALT")
 
 def index(request):
     return HttpResponse("Hello World !")
+
+# @ensure_csrf_cookie
+@csrf_exempt
+def create(request):
+    if request.method == "POST":
+        print(request.POST.get("test"))
+    return 0
+
+@csrf_exempt
+def createAccount(request):
+    # if request.method == "POST":
+    name = request.POST.get("name")
+    pwd = request.POST.get("password")
+    pwd = pwd+salt
+    pwd = hashlib.md5(pwd.encode())
+    print("HASHED : ", pwd.hexdigest()) #WORKING
+    login = Login(name=name, password=pwd)
+    login.save()
+    return 1
+        
+        
 
 # Create your views here.
