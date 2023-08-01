5be7eeca095509e53018fce4d0d6c0ce4cbe5907
manhunter7-2 <thomas.piquet7@gmail.com>
2023-08-01 11:59:43 +0200

    '

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 # exit on error
 set -o errexit
 
+pip install -r requirements.txt
+
+
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 
-pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
